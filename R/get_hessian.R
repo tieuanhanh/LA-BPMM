85b9492287ffe9f7f8d2d2341453c0742025b525
tieuanhanh <40534791+tieuanhanh@users.noreply.github.com>
2023-05-26 20:06:48 +0200

    Delete get_hessian.R
    delete redundant code

--- R/get_hessian.R
+++
@@ -1,6 +0,0 @@
-get_hessian <- function(phi, inv.cov.mat){
-  # return the hessian matrix of log likelihood of model given eta and data
-  # phi: vector of fixed effects and random effects
-  # inv.cov.mat: inverse of covariance matrix of phi which is known given eta
-  t(t)%*%(t*c(exp(t%*%phi))) + inv.cov.mat
-}
