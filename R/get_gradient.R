f48d6baf82c501186cd2629b0f3d2da02996d875
tieuanhanh <40534791+tieuanhanh@users.noreply.github.com>
2023-05-26 20:06:33 +0200

    Delete get_gradient.R
    delete redundant code

--- R/get_gradient.R
+++
@@ -1,6 +0,0 @@
-get_gradient <- function(phi, inv.cov.mat){
-  # return the gradient vector of log likelihood of model given eta and data
-  # phi: vector of fixed effects and random effects
-  # inv.cov.mat: inverse of covariance matrix of phi which is known given eta
-  - colSums(t*y - t*c(exp(t%*%phi))) + inv.cov.mat%*%phi
-}
